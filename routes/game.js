b675979ff6aab63db541ef9fe0a46594b6604ecd
Steeve Matou <steeve.matou@gmail.com>
2023-05-03 19:29:51 +0200

    v2

--- routes/game.js
+++ routes/game.js
@@ -3,19 +3,17 @@ const games = require("../games.json");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("index", { games, name: "" });
+  res.render("index", { games, config: "" });
 });
 
-router.get("/game/:nomDuJeu", (req, res) => {
-  const nomDuJeu = req.params.nomDuJeu;
+router.get("/game/:gameName", (req, res) => {
+  const gameName = req.params.gameName;
 
-  const configGame = games.find((jeu) => jeu.name === nomDuJeu);
+  const configGame = games.find((game) => game.name === gameName);
 
-  if (configGame) {
-    res.render(configGame.name, { name: configGame.name });
-  } else {
-    res.status(404).send("Game not found");
-  }
+  configGame
+    ? res.render(configGame.name, { config: configGame })
+    : res.status(404).send("Game not found");
 });
 
 module.exports = router;
